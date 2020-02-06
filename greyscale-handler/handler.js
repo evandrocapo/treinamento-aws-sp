033b17de2c15947c862cc29e34bd79df0a052375
Evandro Capovilla <evandro.junior@venturus.org.br>
2020-02-06 10:48:38 -0300

    added handler

--- greyscale-handler/handler.js
+++ greyscale-handler/handler.js
@@ -1,18 +1,10 @@
 'use strict';
+const greyscaleService = require('./greyscale-service');
 
-module.exports.hello = async event => {
+module.exports.execute = async event => {
+  await greyscaleService.filter(event);
   return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    message: "Filter aplicado.",
+    event
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
