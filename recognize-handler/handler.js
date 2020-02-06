13bc9bfcc086d2bf9b7dfa1dcb9d5b63037fc128
Evandro Capovilla <evandro.junior@venturus.org.br>
2020-02-06 12:13:58 -0300

    added handler

--- recognize-handler/handler.js
+++ recognize-handler/handler.js
@@ -1,18 +1,10 @@
 'use strict';
+const recognizeService = require('./recognize-service');
 
-module.exports.hello = async event => {
+module.exports.execute = async event => {
+  await recognizeService.recognize(event);
   return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    message: "Tag extraidas com Sucesso.",
+    event
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
